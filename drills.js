c92e696c14b4d03c2f249028299fc9946dfb1ce7
Danielle-Russell <67922266+DanielleRussell-beep@users.noreply.github.com>
2020-12-09 12:08:37 -0800

    drills

---
+++ drills.js
@@ -0,0 +1,36 @@
+/*1. What is the Big O for this?
+
+1) O(1) because if you yell out to a crowd someone 
+can answer regardless of crowd size. Didn't need to
+go through and ask people individually 
+
+2) Linear time O(n) since run time is directly proportional
+to number of people to ask. Asking two people will take
+twice as long as asking one person and there's no random direction
+
+*/
+
+/* 
+2. Even or odd
+
+Linear
+
+3. Are you here?
+Polynomial time because it requires to levels of looping
+
+4. Doubler
+Linear time because you have to go through every value to execute
+
+5. Naive search
+Linear time because you have to go through every value to execute
+
+6. Creating Pairs
+Polynomial time because it requires to levels of looping
+
+7. Compute the sequence
+Fibonnaci sequence? Linear time complexity
+
+8. An efficient search
+
+
+*/
